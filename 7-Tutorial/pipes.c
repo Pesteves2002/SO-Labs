186018f57b92a779646996e32b048250838ff3ef
Pesteves2002 <tomasesteves2002@gmail.com>
2022-01-22 17:20:16 +0000

    Add part 1

--- 7-Tutorial/pipes.c
+++ 7-Tutorial/pipes.c
@@ -10,13 +10,16 @@
 
 #define BUFFER_SIZE (128)
 
-int child_main(int rx) {
+void send_msg(int tx, char const *str);
+
+int child_main(int rx, int tx) {
     while (true) {
         char buffer[BUFFER_SIZE];
         ssize_t ret = read(rx, buffer, BUFFER_SIZE - 1);
 
         if (ret == 0) {
             // ret == 0 signals EOF
+
             fprintf(stderr, "[INFO]: parent closed the pipe\n");
             break;
         } else if (ret == -1) {
@@ -28,8 +31,8 @@ int child_main(int rx) {
         fprintf(stderr, "[INFO]: parent sent %zd B\n", ret);
         buffer[ret] = 0;
         fputs(buffer, stdout);
+        send_msg(tx, "GAWK");
     }
-
     close(rx);
     return 0;
 }
@@ -51,12 +54,30 @@ void send_msg(int tx, char const *str) {
     }
 }
 
-int parent_main(int tx) {
+int parent_main(int tx, int rx) {
     // The parent is likes classic rock:
     // https://www.youtube.com/watch?v=lrpXArn3hII
+
+    char buffer[BUFFER_SIZE];
+    ssize_t ret;
+
     send_msg(tx, "Hey little girl\n");
+    sleep(1);
+    ret = read(rx, buffer, BUFFER_SIZE - 1);
+    buffer[ret] = 0;
+    fputs(buffer, stderr);
+
     send_msg(tx, "Is your daddy home?\n");
-    send_msg(tx, "Did he go and leave you all alone?\n");
+    sleep(1);
+    ret = read(rx, buffer, BUFFER_SIZE - 1);
+    buffer[ret] = 0;
+    fputs(buffer, stderr);
+
+    send_msg(tx, "Did he go and leave you all alone? ( ͡o ͜ʖ ͡o)\n");
+    sleep(1);
+    ret = read(rx, buffer, BUFFER_SIZE - 1);
+    buffer[ret] = 0;
+    fputs(buffer, stderr);
 
     fprintf(stderr, "[INFO]: closing pipe\n");
     close(tx);
@@ -68,7 +89,12 @@ int parent_main(int tx) {
 
 int main() {
     int filedes[2];
+    int bruh[2];
     if (pipe(filedes) != 0) {
+        fprintf(stderr, "[ERR]: pipe() failed: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    if (pipe(bruh) != 0) {
         fprintf(stderr, "[ERR]: pipe() failed: %s\n", strerror(errno));
         exit(EXIT_FAILURE);
     }
@@ -77,10 +103,8 @@ int main() {
         // we need to close the ends we are not using
         // otherwise, the child will be perpetually waiting for a message that
         // will never come
-        close(filedes[1]);
-        return child_main(filedes[0]);
+        return child_main(filedes[0], bruh[1]);
     } else {
-        close(filedes[0]);
-        return parent_main(filedes[1]);
+        return parent_main(filedes[1], bruh[0]);
     }
 }
